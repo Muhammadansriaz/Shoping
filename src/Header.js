5c06af030bf53f8b19b33422c1860c0d5803e2c5
muhammadansriaz <ansriaz444@gmail.com>
2021-05-31 17:23:09 +0500

    updated ads

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Header = (props) => {
   return (
@@ -29,53 +29,97 @@ const Header = (props) => {
 
           <div className="d-flex align-items-center">
             <form className="w-100 me-3">
-              <input  
-              onChange={(evt)=>{props.setSearch(evt.target.value)}}
+              <input
+                onChange={(evt) => {
+                  props.setSearch(evt.target.value);
+                }}
                 type="search"
                 className="form-control"
                 placeholder="Find Cars, Mobile Phones and more..."
                 aria-label="Search"
               />
             </form>
-            <button type="button" class="btn btn-outline-dark">Search</button>
+            <button type="button" class="btn btn-outline-dark">
+              Search
+            </button>
           </div>
         </div>
       </header>
 
-
-
-
-     
-    
-
-
-      <div className = "slider"><div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" />
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img height = "350px" src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg" className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img height = "350px" src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg" className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img height = "350px" src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg" className="d-block w-100" alt="..." />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true" />
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true" />
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-</div>
+      <div className="slider">
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to={0}
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to={1}
+              aria-label="Slide 2"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to={2}
+              aria-label="Slide 3"
+            />
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                height="350px"
+                src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                height="350px"
+                src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                height="350px"
+                src="https://d26a57ydsghvgx.cloudfront.net/product/Customer%20Story%20Images/OLX3.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
